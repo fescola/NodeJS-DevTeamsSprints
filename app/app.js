fcf020e30166e10183e3725df32df556d3812120
Ferran Escolà <escolasarabia@gmail.com>
2022-02-14 13:05:40 +0100

    .

--- app/app.js
+++ app/app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var db = require('./routes/db')
 var UserController = require('./controllers/UserController');
+//var server = require('./config/server')
+
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
+
 
 app.use('/', UserController);
-module.exports = app;
+//module.exports = app;
