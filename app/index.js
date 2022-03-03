cad2b8aa5af7f7d7d5695d17703e10c1af46918c
Ferran Escolà <escolasarabia@gmail.com>
2022-03-03 13:00:02 +0100

    chat- auth Register API — To encrypt password todo

---
+++ app/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const authRoutes = require("./routes/auth");
+
+app.use(express.json()); 
+
+// route middlewares
+app.use("/api/user", authRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
+
+  
+mongoose.connect(
+    process.env.DB_CONNECT,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    () => console.log("connected to db")
+    );
+
+    
+/*
+//SOCKET STUFF 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+// this
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+     console.log('message: ' + msg);
+    });
+  });
+  // ^ VERY IMPORTANT CODE ^ 
+
+  io.on('connection', (socket) => {
+    socket.broadcast.emit('hi');
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+  */
---
+++ app/routes/auth.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const { registerValidation } = require("../validation");
+
+// validation
+const Joi = require("@hapi/joi");
+const schema = Joi.object({
+  name: Joi.string().min(6).max(255).required(),
+  email: Joi.string().min(6).max(255).required().email(),
+  password: Joi.string().min(6).max(1024).required(),
+});
+
+router.post("/register", async (req, res) => {
+    const { error } = schema.validate(req.body);
+
+    if(error){
+        return res.status(400).json({error: error.details[0].message});
+    }
+    const isEmailExist = await User.findOne({ email: req.body.email });
+    if (isEmailExist){
+    return res.status(400).json({ error: "Email already exists" });
+    }
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    try {
+      const savedUser = await user.save();
+      res.json({ error: null, data: savedUser });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  });
+
+module.exports = router;
