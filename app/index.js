704c54f30143cb485462e2f72992fe0d7b3b679a
Ferran Escolà <escolasarabia@gmail.com>
2022-03-21 13:08:27 +0100

    small changes

--- app/index.js
+++ app/index.js
@@ -3,10 +3,10 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server,{
-  cors: {
-    origin: "http://127.0.0.1:5501"
-  }
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
 });
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -19,14 +19,14 @@ const controller = require("./controller/controller")
 var cors = require('cors')
 
 app.use(cors())
-app.use(express.json()); 
+app.use(express.json());
 
 
 // route middlewares
 app.use("/api/user", authRoutes);
-app.use("/api/dashboard",verifyToken,dashboardRoutes)
+app.use("/api/dashboard", verifyToken, dashboardRoutes)
 
-app.use('/',routes)
+app.use('/', routes)
 
 // app.get('/', (req, res) => {
 //   res.sendFile(__dirname + '/index.html');
@@ -34,52 +34,59 @@ app.use('/',routes)
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
-  });
+});
 
 mongoose.connect(
-    process.env.DB_CONNECT,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    process.env.DB_CONNECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     },
     () => console.log("connected to db")
-    );
+);
 
 //SOCKET STUFF 
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+        console.log('user disconnected');
     });
-  });
+});
 // this
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-     console.log('message: ' + msg);
+        console.log('message: ' + msg);
     });
-  });
+});
 
-  io.on('connection', (socket) => {
+io.on('connection', (socket) => {
     socket.broadcast.emit('hi');
-  });
+});
 
-  io.on('connection', (socket) => {
+io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-      let time = new Date();
-      let data = {
-        msg: msg,
-        date: time.toLocaleString()
-      }
-      controller.saveMsg(data)
+        io.emit('chat message', msg);
+        let time = new Date();
+        let data = {
+            msg: msg,
+            date: time.toLocaleString()
+        }
+        controller.saveMsg(data)
     });
-  });
+});
+//room creation server side test
+io.sockets.on('connection', function(socket) {
+    socket.on('create', function(room) {
+        socket.join(room);
+        console.log(`joined room: ${socket.rooms}`)
+    });
+});
 
-  // users = [];
-  // io.on('connection', function(socket){
-  //    console.log('A user connected');
-  //    socket.on('setUsername', function(data){
-  //          users.push(data);
-  //          socket.emit('userSet', {username: data});
-  //    })
-  // });
+
+// users = [];
+// io.on('connection', function(socket){
+//    console.log('A user connected');
+//    socket.on('setUsername', function(data){
+//          users.push(data);
+//          socket.emit('userSet', {username: data});
+//    })
+// });
